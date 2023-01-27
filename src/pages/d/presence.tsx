47951257e0594c88bd9ee412ac81d46949235d26
Sandy Raja Alamsyah <sandyrajalamsyah@gmail.com>
2023-01-27 10:47:50 +0700

    chore: move statistic to variable component

--- src/pages/d/presence.tsx
+++ src/pages/d/presence.tsx
@@ -3,37 +3,66 @@ import { Flex, Space, Tabs, Text, Title } from "@mantine/core";
 
 export default function Presensi() {
   const totalStudent = 2013;
-  const presenceStatistic = [
-    {
-      title: "Presensi terisi",
-      count: 1280
-    },
-    {
-      title: "Hadir",
-      count: 653
-    },
-    {
-      title: "Terlambat",
-      count: 12
-    },
-    {
-      title: "Izin / Sakit",
-      count: 62
-    },
-    {
-      title: "Alpha",
-      count: 32
-    }
-  ];
+
+  const StatisticPresences = () => {
+    const presenceStatistic = [
+      {
+        title: "Presensi terisi",
+        count: 1280
+      },
+      {
+        title: "Hadir",
+        count: 653
+      },
+      {
+        title: "Terlambat",
+        count: 12
+      },
+      {
+        title: "Izin / Sakit",
+        count: 62
+      },
+      {
+        title: "Alpha",
+        count: 32
+      }
+    ];
+
+    return (
+      <>
+        {presenceStatistic.map((val, index) => (
+          <Flex
+            key={val.title}
+            maw="fit-content"
+            gap="xs"
+            align="flex-end"
+            bg={index == 0 ? "black" : ""}
+            c={index == 0 ? "white" : ""}
+            px="md"
+            py="sm"
+            sx={(theme) => ({
+              borderRadius: theme.defaultRadius,
+              border: index > 0 ? `1px solid ${theme.colors.dark[0]}` : ""
+            })}>
+            <Flex direction="column">
+              <Text fw={600}>{val.title}</Text>
+              <Title order={6}>
+                {val.count} / {totalStudent}
+              </Title>
+            </Flex>
+            <Text fz="sm">siswa</Text>
+          </Flex>
+        ))}
+      </>
+    );
+  };
 
   return (
     <Dashboard>
       <Flex direction="column" gap="md">
         <Flex direction="column">
-          <Title order={3}>Presensi Hari Ini</Title>
-          <Text fw={600} fz="lg">
-            Jum&#39;at, 13 Januari 2023
-          </Text>
+          <Title order={1}>Presensi Hari Ini</Title>
+          <Text fw={600}>Jum&#39;at, 13 Januari 2023</Text>
         </Flex>
         <Tabs defaultValue="presence">
           <Tabs.List>
@@ -47,31 +76,7 @@ export default function Presensi() {
             <Space h="md" />
             {/* Statistik presensi */}
             <Flex gap="md" wrap="wrap">
-              {presenceStatistic.map((val, index) => (
-                <Flex
-                  key={val.title}
-                  maw="fit-content"
-                  gap="xs"
-                  align="flex-end"
-                  bg={index == 0 ? "black" : ""}
-                  c={index == 0 ? "white" : ""}
-                  px="md"
-                  py="sm"
-                  sx={(theme) => ({
-                    borderRadius: theme.defaultRadius,
-                    border: index > 0 ? `1px solid ${theme.colors.dark[0]}` : ""
-                  })}>
-                  <Flex direction="column">
-                    <Text fw={600}>{val.title}</Text>
-                    <Title order={6}>
-                      {val.count} / {totalStudent}
-                    </Title>
-                  </Flex>
-                  <Text fz="sm">siswa</Text>
-                </Flex>
-              ))}
-
-              <Flex></Flex>
+              <StatisticPresences />
             </Flex>
           </Tabs.Panel>
 
