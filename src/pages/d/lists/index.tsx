40a5a7027b5df0410745fdc8ddbccf3f7f17284f
Sandy Raja Alamsyah <sandyrajalamsyah@gmail.com>
2023-02-08 11:02:43 +0700

    feat: add list admin

--- src/pages/d/lists/index.tsx
+++ src/pages/d/lists/index.tsx
@@ -1,33 +1,47 @@
-import { Button, Flex, Input, Tabs, TextInput, Title } from "@mantine/core";
+import { Button, Flex, Tabs, TextInput, Title } from "@mantine/core";
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import { RiAddFill, RiSearchLine } from "react-icons/ri";
 import Table from "@/components/TableList";
 import Dashboard from "@/layouts/Dashboard";
+import { useToggle } from "@mantine/hooks";
 
 function ListStudent() {
   const [students, setStudents] = useState<Student[]>([]);
+  const [admins, setAdmins] = useState<Manager[]>([]);
+  const [title, toggleTitle] = useToggle<"Siswa" | "Pengurus">([
+    "Siswa",
+    "Pengurus"
+  ] as const);
 
-  const getStudents = async (count: number = 9) => {
-    return await fetch(
-      `https://api.mockaroo.com/api/6fa63520?count=${count}&key=ab26b160`
-    )
+  const getStudents = async () => {
+    return await fetch("https://spps.free.mockoapp.net/students")
       .then((resolve) => resolve.json())
       .then((data) => setStudents(() => data))
-      .catch((err) => console.log("Data siswa tidak bisa diambil", err));
+      .catch((error) => console.log("Data siswa tidak bisa diambil", error));
   };
+
+  const getAdmins = async () => {
+    return await fetch("https://spps.free.mockoapp.net/managers")
+      .then((resolve) => resolve.json())
+      .then((data) => setAdmins(() => data))
+      .catch((error) => console.log("Data siswa tidak bisa diambil", error));
+  };
+
+  const handleTabChange = (value: "Siswa" | "Pengurus") => toggleTitle(value);
 
   useEffect(() => {
     getStudents();
+    getAdmins();
   }, []);
 
   return (
     <Dashboard>
-      <Title order={1}>Daftar Siswa</Title>
-      <Tabs defaultValue="student" mt="md">
+      <Title order={1}>Daftar {title}</Title>
+      <Tabs defaultValue="student" mt="md" onTabChange={handleTabChange}>
         <Tabs.List>
-          <Tabs.Tab value="responsible">Pengurus</Tabs.Tab>
           <Tabs.Tab value="student">Siswa</Tabs.Tab>
+          <Tabs.Tab value="manager">Pengurus</Tabs.Tab>
         </Tabs.List>
         <Tabs.Panel value="student" mt="md">
           <Flex direction="column" gap="md">
@@ -45,15 +59,11 @@ function ListStudent() {
                 <Button variant="outline" component={Link} href="/d/lists/edit">
                   Edit
                 </Button>
-                <Button variant="outline" disabled>
-                  Reset Info Perangkat
-                </Button>
               </Flex>
               <TextInput icon={<RiSearchLine />} placeholder="Cari siswa..." />
             </Flex>
             <Table
               data={students}
-              unique="NIS"
               ths={[
                 "No. Absen",
                 "Nama Lengkap",
@@ -66,8 +76,39 @@ function ListStudent() {
           </Flex>
         </Tabs.Panel>
 
-        <Tabs.Panel value="responsible" mt="md">
-          Pengurus
+        <Tabs.Panel value="manager" mt="md">
+          <Flex direction="column" gap="md">
+            <Flex justify="space-between" mr="xl">
+              <Flex gap="sm">
+                <Button
+                  leftIcon={<RiAddFill />}
+                  component={Link}
+                  href="/d/lists/add">
+                  Tambah Pengurus
+                </Button>
+                <Button variant="outline" disabled>
+                  Hapus
+                </Button>
+                <Button variant="outline" component={Link} href="/d/lists/edit">
+                  Edit
+                </Button>
+              </Flex>
+              <TextInput
+                icon={<RiSearchLine />}
+                placeholder="Cari pengurus..."
+              />
+            </Flex>
+            <Table
+              data={admins}
+              ths={[
+                "Nama Lengkap",
+                "NIP",
+                "Akses Kelas",
+                "Peran",
+                "No. Telepon"
+              ]}
+            />
+          </Flex>
         </Tabs.Panel>
       </Tabs>
     </Dashboard>
