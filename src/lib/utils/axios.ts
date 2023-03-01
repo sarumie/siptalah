1f5ada83e3aa7d3c23681dd6dbd44857b39fd6a1
Sandy Raja Alamsyah <sandyrajalamsyah@gmail.com>
2023-03-01 21:30:46 +0700

    chore: init axios

---
+++ src/lib/utils/axios.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3000/api/",
+  timeout: 10000,
+  headers: { "X-Custom-Header": "foobar" }
+});
+
+export default axiosInstance;
