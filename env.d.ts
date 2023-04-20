f23798c5abfe0750e7a802693a3b4f439afb1e3d
Sandy <sandyrajalamsyah@gmail.com>
2023-04-20 01:01:46 +0000

    chore: add env key

--- env.d.ts
+++ env.d.ts
@@ -2,8 +2,13 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NEXT_PUBLIC_BASE_URL: string;
+      DATABASE_URL: string;
+      SHADOW_DATABASE_URL: string;
+      SUPABASE_KEY: string;
+      SUPABASE_URL: string;
     }
   }
 }
 
 export {};
+
