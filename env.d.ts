d4515e2ea7871820fbcd5ca4ba6d50f5125cc2fd
Sandy <sandyrajalamsyah@gmail.com>
2023-05-05 02:11:34 +0000

    chore: add env key

--- env.d.ts
+++ env.d.ts
@@ -2,10 +2,12 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NEXT_PUBLIC_BASE_URL: string;
+      NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+      NEXT_PUBLIC_SUPABASE_URL: string;
       DATABASE_URL: string;
       SHADOW_DATABASE_URL: string;
-      SUPABASE_KEY: string;
-      SUPABASE_URL: string;
+      NODE_ENV: "development" | "production";
+      PORT?: string;
     }
   }
 }
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,4 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-export default createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+
+export default supabase;
 
