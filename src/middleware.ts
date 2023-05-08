87868242fd1ea12df1d35eb24a7f7e44151a355d
Sandy <sandyrajalamsyah@gmail.com>
2023-05-08 08:43:20 +0000

    feat: verify a session on middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,49 @@
-import { NextResponse as res } from "next/server";
+import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import jwt from "jsonwebtoken";
-import { readFileSync } from "fs";
+import { createMiddlewareSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { baseUrl } from "./lib/utils";
 
-export function middleware(req: NextRequest) {
-  // const { origin, pathname: path } = req.nextUrl;
-  // const authToken = req.cookies.get("auth_token");
-  // if (path.startsWith("/login") || path.startsWith("/register")) {
-  //   if (authToken) return res.redirect(new URL("/d/presensi", origin));
-  //   return;
-  // }
-  // if (path.startsWith("/api/auth/")) return;
-  // if (!authToken) return res.redirect(new URL("/login", origin));
-  // const token = jwt.verify(authToken.value, readFileSync("private_key.pem"));
-  // * Example for return a response
-  //   return res.next({
-  //     status: 405,
-  //     statusText: "Method not allowed"
-  //   });
+export async function middleware(req: NextRequest) {
+  const { pathname: path } = req.nextUrl;
+  const supabase = createMiddlewareSupabaseClient({
+    req,
+    res: NextResponse.next()
+  });
+
+  const {
+    data: { session }
+  } = await supabase.auth.getSession();
+
+  if (
+    (path.startsWith("/api/auth") || path.startsWith("/login")) &&
+    !path.startsWith("/api/auth/logout") &&
+    !session
+  ) {
+    const redirectUrl = req.nextUrl.clone();
+    redirectUrl.pathname = "/d/presensi";
+    redirectUrl.searchParams.set("redirected_from", req.nextUrl.pathname);
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  if (!session) {
+    const redirectUrl = req.nextUrl.clone();
+    redirectUrl.pathname = "/login";
+    redirectUrl.searchParams.set("redirected_from", req.nextUrl.pathname);
+    return NextResponse.redirect(redirectUrl);
+  }
 }
+
+// const authToken = req.cookies.get("auth_token");
+// if (path.startsWith("/login") || path.startsWith("/register")) {
+//   if (authToken) return res.redirect(new URL("/d/presensi", origin));
+//   return;
+// }
+// if (path.startsWith("/api/auth/")) return;
+// if (!authToken) return res.redirect(new URL("/login", origin));
+// const token = jwt.verify(authToken.value, readFileSync("private_key.pem"));
+// * Example for return a response
+//   return res.next({
+//     status: 405,
+//     statusText: "Method not allowed"
+//   });
 
