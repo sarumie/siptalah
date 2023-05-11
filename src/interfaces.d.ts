9d183f0c33190539c13a066cc4ccf366b52a302c
Sandy <sandyrajalamsyah@gmail.com>
2023-05-11 03:26:05 +0000

    chore: remove db types uses prisma type instead

--- src/interfaces.d.ts
+++ src/interfaces.d.ts
@@ -1,51 +1,5 @@
-/**
- * UTILITIES
- */
-
 type NonEmptyArray<T> = [T, ...T[]];
 
-//
-
-interface Student {
-  id: string;
-  absent: number;
-  full_name: string;
-  class: string;
-  NIS: number;
-  phone: number;
-}
-interface Manager {
-  id: string;
-  NIP: string;
-  access: string;
-  role: string;
-  phone: string;
-}
-
-interface Presence {
-  id: string;
-  absent: string;
-  fullName: string;
-  NIS: string;
-  class: string;
-  status: string;
-}
-
-interface Major {
-  id: string;
-  name: string;
-  acronym: string;
-  classTotal: string;
-  groupTotal: string;
-}
-
-interface PresenceHistory {
-  id: string;
-  absent: string;
-  date: string;
-  timeRange: string;
-  attend: string;
-  late: string;
-  permit: string;
-  alpha: string;
-}
+type DefaultAPIBodyResponse = {
+  message: string;
+};
